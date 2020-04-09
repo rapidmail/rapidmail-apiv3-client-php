e11a909d2752b58fbc26368936fee8c033f3caf3
Jan-Simon Winkelmann <jwinkelmann@rapidsoft.de>
2020-04-09 20:33:46 +0200

    Delete correct zip archive in build script

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@
 
 BUILDDIR=$(dirname "$0")
 cd "${BUILDDIR}/../"
-rm -rf vendor/ apiv3-client-php.zip
+rm -rf vendor/ rapidmail-apiv3-client-php.zip
 composer install --no-dev
 git archive --format=zip -o rapidmail-apiv3-client-php.zip HEAD \
 && zip -ur rapidmail-apiv3-client-php.zip vendor
